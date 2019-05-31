200c8a575bbaa73b50bd0e1e420ba74768944890
Viktor <viktor.kochegarov@gmail.com>
2019-05-31 21:12:46 +0300

    добавили кнопку наверх на jquery

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,27 @@ $(document).ready(function(){
   });
 
 });
+
+// кнопка наверх
+$(document).ready(function(){
+  /**
+   * При прокрутке страницы, показываем или срываем кнопку
+   */
+  $(window).scroll(function () {
+      // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+      if ($(this).scrollTop() > 50) {
+          $('#button-up').fadeIn();
+      } else {
+          $('#button-up').fadeOut();
+      }
+  });
+  
+  /** При нажатии на кнопку мы перемещаемся к началу страницы */
+  $('#button-up').click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500);
+      return false;
+  });
+  
+});
