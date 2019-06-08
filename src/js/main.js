3953058939a883d86863a1459bfb7284feacb318
Viktor <viktor.kochegarov@gmail.com>
2019-06-08 21:10:08 +0300

    анимация с помощью @keyframes

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ var button = document.querySelector('#button')
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
+
+
 button.addEventListener('click', function(){
   modal.classList.add('modal_active');
 });
