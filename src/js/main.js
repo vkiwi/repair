485989669233d2738d4ef8374c9e785b91ab3f4b
Viktor <viktor.kochegarov@gmail.com>
2019-06-13 17:50:09 +0300

    оптимизация карты

--- src/js/main.js
+++ src/js/main.js
@@ -18,11 +18,24 @@ function autoClose(){
 
 setInterval(autoClose, 5000);
 
-// $(document).ready(function(){
+$(document).ready(function(){
 //   $('h2').css('border', 'solid 3px red');
 // });
 
 // // p нет, абзатцев нет на странице
 // $(function(){
 //   $('p').css('border', 'solid 3px red');
-// });
+
+// показывать карту только когда докрутили до 
+
+  var brif = $('.steps');
+  var brifTop = brif.offset().top;
+  $(window).bind('scroll', function(){
+    var windowTop = $(this).scrollTop();
+    if (windowTop >= brifTop) {
+      console.log('докрутили');
+      $('#map').html('<script async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A77b7da777b7b914974b60677f22834c6abc6dc155c3e364c5b83576b324c1bbd&amp;width=100%25&amp;height=640&amp;lang=ru_RU&amp;scroll=false"></script>');
+      $(window).unbind('scroll');
+    }
+  });
+});
